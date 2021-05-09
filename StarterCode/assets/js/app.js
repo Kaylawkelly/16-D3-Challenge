2c2cde019d6b28209184434c0f416bba02427d42
Kayla Kelly <76846236+Kaylawkelly@users.noreply.github.com>
2021-05-09 08:26:38 -0400

    commit

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -19,3 +19,12 @@ function makeResponsive() {
     chartWidth = svgWidth - chartMargin.left - chartMargin.right;
     chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
     refreshChart();
+}
+
+function createSVG() {
+    svg = d3.select("#scatter").append("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
+    chartGroup = svg.append("g")
+        .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+}
