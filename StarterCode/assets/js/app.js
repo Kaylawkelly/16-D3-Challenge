f79c7307b72c566db43f3e3d6918101b93240de1
Kayla Kelly <76846236+Kaylawkelly@users.noreply.github.com>
2021-05-09 08:20:55 -0400

    commit

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -5,4 +5,10 @@ var chartMargin = {
     left: 100,
     right: 20,
     bottom: 100
+};
 
+var chartWidth, chartHeight;
+var xDefault = "poverty",
+    yDefault = "obesity";
+var axis_values = { x: xDefault, y: yDefault };
+var svg, chartGroup;
